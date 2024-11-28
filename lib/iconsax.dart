84071b744c23d61ce3235752a1d522891a531300
Antonio Tornesello <antoniotornesello97@gmail.com>
2024-11-28 18:08:05 +0100

    feat(initial_commit): refactored all iconsax set icons

---
+++ lib/iconsax.dart
@@ -0,0 +1,8 @@
+library iconsax;
+
+export 'icons/iconsax_bold_icons.dart' show IconsaxBold;
+export 'icons/iconsax_broken_icons.dart' show IconsaxBroken;
+export 'icons/iconsax_bulk_icons.dart' show IconsaxBulk;
+export 'icons/iconsax_linear_icons.dart' show IconsaxLinear;
+export 'icons/iconsax_outline_icons.dart' show IconsaxOutline;
+export 'icons/iconsax_two_tone_icons.dart' show IconsaxTwotone;
